b0ad88bf08f6f670770c84a983eb737bc50c517e
Janet <janet@tirisan.co.za>
2024-10-09 12:53:31 +0200

    Added inputs and buttons on add damage house images page

--- src/app/add-damage-house-images/add-damage-house-images.page.ts
+++ src/app/add-damage-house-images/add-damage-house-images.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-damage-house-images',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-damage-house-images.page.scss'],
 })
 export class AddDamageHouseImagesPage implements OnInit {
-
-  constructor() { }
+  //variables
+  url:any
+  constructor(public alertCtrl:AlertController) { }
 
   ngOnInit() {
   }
+ async insertImagine(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
 
+      if (event.target.files[0].size > 1500000) {
+        const alert =await  this.alertCtrl.create({
+          header: "Oh no!",
+          message: "your photo is too large, please choose a photo with 1.5MB or less.",
+          buttons: ['OK'],
+          cssClass: "myAlert",
+        });
+      await alert.present();
+      }
+      else {
+        reader.onload = (event: any) => {
+          this.url = event.target.result;
+        }
+        reader.readAsDataURL(event.target.files[0]);
+      }
+    }
+  }
 }
